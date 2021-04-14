a834c8d6991785ce97e3b0bbee37b130c9e4ce34
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2021-04-14 19:59:53 +0700

    Page-header - верстка и JS

---
+++ source/js/mobile-menu.js
@@ -0,0 +1,14 @@
+const pageHeader = document.querySelector('.page-header');
+console.log(pageHeader);
+const navToggle = pageHeader.querySelector('.page-header__toggle');
+
+navToggle.addEventListener('click', function() {
+    if (pageHeader.classList.contains('page-header__menu-closed')) {
+    pageHeader.classList.remove('page-header__menu-closed');
+    pageHeader.classList.add('page-header__menu-opened');
+  } else {
+    pageHeader.classList.add('page-header__menu-closed');
+    pageHeader.classList.remove('page-header__menu-opened');
+  }
+});
+
