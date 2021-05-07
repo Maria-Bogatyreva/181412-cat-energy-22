44efa0113b3e5f64c6280df982729459df89890c
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2021-05-07 15:00:25 +0700

    п.3.7 ТЗ

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 const pageHeader = document.querySelector('.page-header');
 const navToggle = pageHeader.querySelector('.page-header__toggle');
 
+pageHeader.classList.remove('page-header__menu-opened');
+pageHeader.classList.add('page-header__menu-closed');
+
 navToggle.addEventListener('click', function() {
     if (pageHeader.classList.contains('page-header__menu-closed')) {
     pageHeader.classList.remove('page-header__menu-closed');
