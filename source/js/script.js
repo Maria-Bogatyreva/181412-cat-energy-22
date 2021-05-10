b4b059b1cf28c214565639938873185df2ba650f
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2021-05-10 17:31:54 +0700

    Типа, слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,22 @@ navToggle.addEventListener('click', function() {
     pageHeader.classList.remove('page-header--menu-opened');
   }
 });
+
+/*Типа, слайдер*/
+const btnBefore = document.querySelector('.slider__button--before');
+const btnAfter = document.querySelector('.slider__button--after');
+
+const imgBefore = document.querySelector('.slider__img--before');
+const imgAfter = document.querySelector('.slider__img--after');
+
+btnAfter.addEventListener('click', function () {
+  imgBefore.classList.add('slider__img--hide');
+  imgAfter.classList.remove('slider__img--hide');
+});
+
+btnBefore.addEventListener('click', function () {
+  imgAfter.classList.add('slider__img--hide');
+  imgBefore.classList.remove('slider__img--hide');
+});
+
+
