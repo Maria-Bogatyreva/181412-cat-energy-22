1bc205021bf87b1f7539573138f15468744ed655
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2021-05-10 17:02:56 +0700

    Исправлена ошибка БЭМ

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 const pageHeader = document.querySelector('.page-header');
 const navToggle = pageHeader.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header__menu-no-js');
-pageHeader.classList.add('page-header__menu-closed');
+pageHeader.classList.remove('page-header--menu-no-js');
+pageHeader.classList.add('page-header--menu-closed');
 
 navToggle.addEventListener('click', function() {
-    if (pageHeader.classList.contains('page-header__menu-closed')) {
-    pageHeader.classList.remove('page-header__menu-closed');
-    pageHeader.classList.add('page-header__menu-opened');
+    if (pageHeader.classList.contains('page-header--menu-closed')) {
+    pageHeader.classList.remove('page-header--menu-closed');
+    pageHeader.classList.add('page-header--menu-opened');
   } else {
-    pageHeader.classList.add('page-header__menu-closed');
-    pageHeader.classList.remove('page-header__menu-opened');
+    pageHeader.classList.add('page-header--menu-closed');
+    pageHeader.classList.remove('page-header--menu-opened');
   }
 });
